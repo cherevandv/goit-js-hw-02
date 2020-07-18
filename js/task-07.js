8341ed500f370cd8ba9772a738aea419a4d828fb
daria kobekhno <cherevandv@mail.com>
2020-07-18 05:52:00 +0300

    added task-07

--- js/task-07.js
+++ js/task-07.js
@@ -3,20 +3,29 @@
 const logins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
 
 const isLoginValid = function (login) {
-  // твой код
+  return login.length >= 4 && login.length <= 16;
 };
 
 const isLoginUnique = function (allLogins, login) {
-  // твой код
+  return allLogins.includes(login);
 };
 
 const addLogin = function (allLogins, login) {
-  // твой код
+  console.log(allLogins, login);
+
+  if (isLoginValid(login) === false) {
+    return "Ошибка! Логин должен быть от 4 до 16 символов";
+  } else if (isLoginUnique(allLogins, login) === true) {
+    return "Такой логин уже используется!";
+  }
+  allLogins.push(login);
+  return "Логин успешно добавлен!";
 };
 
 /*
- * Вызовы функции для проверки работоспособности твоей реализации.
+ * Вызовы функции для проверки работоспособности реализации:
  */
+
 console.log(addLogin(logins, "Ajax")); // 'Логин успешно добавлен!'
 console.log(addLogin(logins, "robotGoogles")); // 'Такой логин уже используется!'
 console.log(addLogin(logins, "Zod")); // 'Ошибка! Логин должен быть от 4 до 16 символов'
